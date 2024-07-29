1a27d07b6e4d8ffdb1313c3861afa08bbb6f0684
Bob3x <bob3xx@protonmail.com>
2024-07-29 20:05:17 +0300

    Practise Git

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
 let pokemonList=[{name: "Balbasure", height: 0.7, type: "grass"},{name: "Charizard", heigt: 1.7, type: ['fire', 'flying'] },{name: "Ivysour", height: 1, type: ['grass', 'poison']},{name: "Venusour", height: 2, type: ['grass', 'poison']},{name: "Charmander", height: 0.6, type: fire}];
+let pokemonDoDaGit;
