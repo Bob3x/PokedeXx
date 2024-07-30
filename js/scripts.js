7316ea8b6fd4734de06c853718e531e6e8c6d7fb
Bob3x <bob3xx@protonmail.com>
2024-07-30 16:44:39 +0300

    For loops in PokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
-let pokemonList=[{name: "Balbasure", height: 0.7, type: "grass"},{name: "Charizard", heigt: 1.7, type: ['fire', 'flying'] },{name: "Ivysour", height: 1, type: ['grass', 'poison']},{name: "Venusour", height: 2, type: ['grass', 'poison']},{name: "Charmander", height: 0.6, type: fire}];
-let pokemonDoDaGit;
+let pokemonList = [
+    { name: "Balbasure", height: 0.7, type: "grass" },
+    { name: "Charizard", heigt: 1.7, type: ['fire', 'flying'] },
+    { name: "Ivysour", height: 1, type: ['grass', 'poison'] },
+    { name: "Venusour", height: 2, type: ['grass', 'poison'] },
+    { name: "Charmander", height: 0.6, type: fire }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+        document.write (pokemon.name + pokemon.height);
+    if (pokemon.height > 1) {
+        document.write (' - Wow, that is huge! ');
+    }
+}
+
