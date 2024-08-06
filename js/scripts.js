a83d87552d7c64c63b8bdd2aa3579b04e23bb013
Bob3x <bob3xx@protonmail.com>
2024-08-06 20:29:33 +0300

    Modal element added

--- js/scripts.js
+++ js/scripts.js
@@ -79,14 +79,131 @@ let pokemonRepository = (function () {
         loadList: loadList,
         loadDetails: loadDetails,
         showDetails: showDetails
-    };
+    };   
 })();
+
+let formValidation = (function() {
+    let form = document.querySelector("#register-form");
+    let emailInput = document.querySelector("#email");
+    let passwordInput = document.querySelector("#password");
+
+    function showErrorMessage(input, message) {
+        let container = input.parentElement; // .input-wrapper
+
+        let error = container.querySelector(".error-message");
+        if (error) {
+            container.removeChild(error);
+        } // check and remove any existing errors 
+
+        if (message) {
+            let error = document.createElement ("div");
+            error.classList.add("error-message");
+            error.innerText = message;
+            container.appendChild(error);
+        } // now add the error if the message isn't empty
+    }
+
+    function validateEmail() {
+        // let value = emailInput.value;
+        // let hasAtSign = value.indexOf("@") > -1;
+        // let hasDot = value.indexOf(".") > -1;
+        // return value && hasAtSign && hasDot;
+        
+        if (!value) { // if value is NOT true 
+            showErrorMessage(emailInput, "Email is a required field.");
+            return false;
+        }
+
+        if (value.indexOf("@") === -1) {
+            showErrorMessage(emailInput, "You must enter a valid email address.");
+            return false;
+        }
+
+        if (value.indexOf(".") === -1) {
+            showErrorMessage(emailInput, "You must enter a valid email address.");
+            return false;
+        }
+
+        showErrorMessage(emailInput, null);
+        return true;
+    }
+
+    function validatePassword() {
+        // let value = passwordInput.value;
+        // return value && value.length >= 8;
+        
+        if (!value) {
+            showErrorMessage(passwordInput, "Password is a required field.");
+            return false;
+        }
+
+        if (value.length < 8) {
+            showErrorMessage(passwordInput, "The password needs to be at least 8 characters.");
+            return false;
+        }
+
+        showErrorMessage(passwordInput, null);
+        return true;
+    }
+
+    function validateForm() {
+        let isValidEmail = validateEmail();
+        let isValidPassword = validatePassword();
+        return isValidEmail && isValidPassword;
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // Do not submit to the server
+        if (validateForm()) {
+            alert("Success!");
+        }
+    });
+    
+    emailInput.addEventListener("input", validateEmail);
+    passwordInput.addEventListener("input", validatePassword);
+})()
+
+let modalShow = (function () {
+
+    function showModal(tittle, text) {
+        let modalContainer = document.querySelector("#modal-container");
+        modalContainer.innerHTML = "";
+
+        let modal = document.createElement("div");
+        modal.classList.add("modal");
+
+        ;et closeButtonElement = document.createElement("button");
+        closeButtonElement.classList.add("modal-close");
+        closeButtonelement.innerText = "Cloase";
+
+        let tittleElement = document.createElement("h1");
+        tittleElement.innerText = tittle;
+
+        let contentElement = document.createElement("p");
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(tittleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add("is-visible");
+    }
+
+    document.querySelector("#show-modal").addEventListener("click", () => {
+        showModal("Modal tittle", "This is the modal content!");
+    });
+})();
+
+
 
 pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
 });
+
+
 
 
 
