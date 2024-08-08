8d40ae426dcb4da9349cd753511554be404601a0
Bob3x <bob3xx@protonmail.com>
2024-08-08 21:39:46 +0300

    Fetching pokemon picture

--- js/scripts.js
+++ js/scripts.js
@@ -72,8 +72,6 @@ let pokemonRepository = (function () {
         });
     }
 
-
-
     function showModal(title, text, img) {
         
         modalContainer.innerHTML = "";
@@ -96,9 +94,7 @@ let pokemonRepository = (function () {
         pokemonImage.setAttribute("src", img);
         pokemonImage.setAttribute("width", "100%");
         pokemonImage.setAttribute("height", "100%");
-        // pokemonImage.src = pokemonImage.imageUrl;
-        
-        
+          
         modal.appendChild(closeButtonElement);
         modal.appendChild(titleElement);
         modal.appendChild(contentElement);
@@ -128,90 +124,13 @@ let pokemonRepository = (function () {
             }
         });
     }
-
-    // function formValidate() {
-    //     let form = document.querySelector("#register-form");
-    //     let emailInput = document.querySelector("#email");
-    //     let passwordInput = document.querySelector("#password");
-
-    // function validateEmail() {
-    //         if (!value) { // if value is NOT true 
-    //             showErrorMessage(emailInput, "Email is a required field.");
-    //             return false;
-    //         }
-
-    //         if (value.indexOf("@") === -1) {
-    //             showErrorMessage(emailInput, "You must enter a valid email address.");
-    //             return false;
-    //         }
-
-    //         if (value.indexOf(".") === -1) {
-    //             showErrorMessage(emailInput, "You must enter a valid email address.");
-    //             return false;
-    //         }
-
-    //         showErrorMessage(emailInput, null);
-    //         return true;
-    //     }
-    //     emailInput.addEventListener("input", validateEmail);
-
-    // function validatePassword() {
-    //         // let value = passwordInput.value;
-    //         // return value && value.length >= 8;
-
-    //         if (!value) {
-    //             showErrorMessage(passwordInput, "Password is a required field.");
-    //             return false;
-    //         }
-
-    //         if (value.length < 8) {
-    //             showErrorMessage(passwordInput, "The password needs to be at least 8 characters.");
-    //             return false;
-    //         }
-
-    //         showErrorMessage(passwordInput, null);
-    //         return true;
-    //     }
-    //     passwordInput.addEventListener("input", validatePassword);
-
-    // function showErrorMessage(input, message) {
-    //         let container = input.parentElement; // .input-wrapper
-
-    //         let error = container.querySelector(".error-message");
-    //         if (error) {
-    //             container.removeChild(error);
-    //         } // check and remove any existing errors 
-
-    //         if (message) {
-    //             let error = document.createElement("div");
-    //             error.classList.add("error-message");
-    //             error.innerText = message;
-    //             container.appendChild(error);
-    //         }
-    //     } // now add the error if the message isn't empty
-
-    // function validateForm() {
-    //         let isValidEmail = validateEmail();
-    //         let isValidPassword = validatePassword();
-    //         return isValidEmail && isValidPassword;
-    //     }
-
-    //     form.addEventListener("submit", (e) => {
-    //         e.preventDefault(); // Do not submit to the server
-    //         if (validateForm()) {
-    //             alert("Success!");
-    //         }
-    //     });
-    // }
-
-    return {
+     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
         showDetails: showDetails,
         loadList: loadList,
-        loadDetails: loadDetails,
-
+        loadDetails: loadDetails
     }
 })();
       
@@ -221,46 +140,5 @@ pokemonRepository.loadList().then(function() {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < pokemonList.length; i++) {
-//     let pokemon = pokemonList[i];
-//         document.write ('<p>' + pokemon.name + ' (height: '+ pokemon.height +')');
-//     if (pokemon.height > 1.7) {
-//         document.write ('<strong> - Wow, that is huge! </strong>');
-//     }
-// }
-
-// pokemonList.forEach(function(pokemon) {
-//     document.write ('<p>' + pokemon.name + " " + "is" + " " + pokemon.height + "m" );
-
-// })
-
-// function PokemonLoop(pokemon) {
-//     document.write ('<p>' + pokemon.name + " " + "is" + " " + pokemon.height + "m" );
-
-// }
-
- 
-
-
-// function divide (dividend, divisor){
-//     if (divisor === 0){
-//         return "You're trying to divide by zero."
-//     }else{
-//         let result = dividend / divisor;
-//         return result;   
-//     }
-//     document.write(divide(4,2));
-// }
 
 
